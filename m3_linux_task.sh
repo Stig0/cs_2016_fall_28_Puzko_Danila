f532989dcec6f1780619d42de609039f2561a696
Stig0 <danilapuzko@mail.ru>
2016-11-17 19:22:26 +0300

    Update m3_linux_task.sh

--- m3_linux_task.sh
+++ m3_linux_task.sh
@@ -1,4 +1,4 @@
 wget 2700chess.com
 cat index.html | grep -o 'players/[a-z\+\-]\+">[A-Z][a-z]\+' > rating.txt
-sed -e 's/players[/][a-z\+\-]\+">//g' rating > rating1
-cat -n rating1; rm index.html
+sed -e 's/players[/][a-z\+\-]\+">//g' rating.txt > rating1.txt
+cat -n rating1.txt; rm index.html
