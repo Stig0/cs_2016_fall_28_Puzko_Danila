597791f9a2801e62462e23779d81484731879185
Stig0 <danilapuzko@mail.ru>
2017-03-23 20:42:50 +0300

    Create 12.c

---
+++ m1_2017/12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i=0;
+    char c[1000];
+    int max=c[0];
+    printf("vvedite stroku:");
+    gets(c);
+    for(i=0;c[i]!='\0';++i){
+
+        if(c[i+1]>max){
+            max=c[i+1];
+
+        }
+
+        else{
+            max=max;
+        }
+    }
+
+    printf("%c", max);
+
+}
