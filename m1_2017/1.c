5a14ec962d57c49461d4c7754724460355c4b49b
Stig0 <danilapuzko@mail.ru>
2017-03-24 22:05:37 +0300

    Update 1.c

--- m1_2017/1.c
+++ m1_2017/1.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello, Mosque!\n");
+    int day;
+    int month;
+    int t;
+    day=11;
+    month=07;
+    printf("month=%d, month=%d\n", month, day);
+
+
+    printf("month=%d, day=%d", day, month);
     return 0;
 }
